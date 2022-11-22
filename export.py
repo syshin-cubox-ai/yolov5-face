86eab53b055de46fecb567e38313a84d0173767c
Seokyong Shin <syshin@cubox.ai>
2022-11-22 16:44:56 +0900

    내 코드로 변경

--- export.py
+++ export.py
@@ -5,42 +5,39 @@ Usage:
 """
 
 import argparse
-import sys
-import time
+import os
 
-sys.path.append('./')  # to run '$ python *.py' files in subdirectories
-
+import numpy as np
+import onnx.shape_inference
+import onnxruntime.tools.symbolic_shape_infer
+import onnxsim
 import torch
 import torch.nn as nn
 
-import models
+import models.common
 from models.experimental import attempt_load
 from utils.activations import Hardswish, SiLU
-from utils.general import set_logging, check_img_size
-import onnx
+from utils.general import check_img_size
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default='./yolov5s.pt', help='weights path')  # from yolov5/models/
+    parser.add_argument('--weights', type=str, default='weights/yolov5s-face.pt', help='weights path')
     parser.add_argument('--img_size', nargs='+', type=int, default=[640, 640], help='image size')  # height, width
     parser.add_argument('--batch_size', type=int, default=1, help='batch size')
-    parser.add_argument('--dynamic', action='store_true', default=False, help='enable dynamic axis in onnx model')
-    parser.add_argument('--onnx2pb', action='store_true', default=False, help='export onnx to pb')
-    parser.add_argument('--onnx_infer', action='store_true', default=True, help='onnx infer test')
-    #=======================TensorRT=================================
-    parser.add_argument('--onnx2trt', action='store_true', default=False, help='export onnx to tensorrt')
-    parser.add_argument('--fp16_trt', action='store_true', default=False, help='fp16 infer')
-    #================================================================
+    parser.add_argument('--dynamic', action='store_true', help='enable dynamic axis in onnx model')
+    parser.add_argument('--skip_simplify', action='store_true', help='skip onnx-simplifier')
+    # =======================TensorRT=================================
+    parser.add_argument('--onnx2trt', action='store_true', help='export onnx to tensorrt')
+    parser.add_argument('--fp16_trt', action='store_true', help='fp16 infer')
+    # ================================================================
     opt = parser.parse_args()
     opt.img_size *= 2 if len(opt.img_size) == 1 else 1  # expand
     print(opt)
-    set_logging()
-    t = time.time()
 
     # Load PyTorch model
     model = attempt_load(opt.weights, map_location=torch.device('cpu'))  # load FP32 model
     delattr(model.model[-1], 'anchor_grid')
-    model.model[-1].anchor_grid=[torch.zeros(1)] * 3 # nl=3 number of detection layers
+    model.model[-1].anchor_grid = [torch.zeros(1)] * 3  # nl=3 number of detection layers
     model.model[-1].export_cat = True
     model.eval()
     labels = model.names
@@ -62,81 +59,76 @@ if __name__ == '__main__':
                 m.act = SiLU()
         # elif isinstance(m, models.yolo.Detect):
         #     m.forward = m.forward_export  # assign forward (optional)
-        if isinstance(m, models.common.ShuffleV2Block):#shufflenet block nn.SiLU
+        if isinstance(m, models.common.ShuffleV2Block):  # shufflenet block nn.SiLU
             for i in range(len(m.branch1)):
                 if isinstance(m.branch1[i], nn.SiLU):
                     m.branch1[i] = SiLU()
             for i in range(len(m.branch2)):
                 if isinstance(m.branch2[i], nn.SiLU):
                     m.branch2[i] = SiLU()
-    y = model(img)  # dry run
 
-    # ONNX export
-    print('\nStarting ONNX export with onnx %s...' % onnx.__version__)
-    f = opt.weights.replace('.pt', '.onnx')  # filename
-    model.fuse()  # only for ONNX
-    input_names=['input']
-    output_names=['output']
-    torch.onnx.export(model, img, f, verbose=False, opset_version=12, 
+    # Define output file path
+    output_dir = 'onnx_files'
+    os.makedirs(output_dir, exist_ok=True)
+    output_path = os.path.join(output_dir, os.path.basename(opt.weights).replace('.py', '.onnx'))
+
+    # Define input and output names
+    input_names = ['img']
+    output_names = ['pred']
+
+    # Define dynamic_axes
+    if opt.dynamic:
+        dynamic_axes = {input_names[0]: {0: 'N', 2: 'H', 3: 'W'},
+                        output_names[0]: {0: 'N', 1: 'Candidates', 2: 'dyn_16'}}
+    else:
+        dynamic_axes = None
+
+    # Export model into ONNX format
+    torch.onnx.export(
+        model,
+        img,
+        output_path,
         input_names=input_names,
         output_names=output_names,
-        dynamic_axes = {'input': {0: 'batch'},
-                        'output': {0: 'batch'}
-                        } if opt.dynamic else None)
+        opset_version=17,
+        dynamic_axes=dynamic_axes,
+    )
 
-    # Checks
-    onnx_model = onnx.load(f)  # load onnx model
-    onnx.checker.check_model(onnx_model)  # check onnx model
-    print('ONNX export success, saved as %s' % f)
-    # Finish
-    print('\nExport complete (%.2fs). Visualize with https://github.com/lutzroeder/netron.' % (time.time() - t))
+    # Check exported onnx model
+    onnx_model = onnx.load(output_path)
+    onnx.checker.check_model(onnx_model)
+    try:
+        onnx_model = onnxruntime.tools.symbolic_shape_infer.SymbolicShapeInference.infer_shapes(onnx_model)
+        onnx.save(onnx_model, output_path)
+    except Exception as e:
+        print(f'ERROR: {e}, skip symbolic shape inference.')
+    onnx.shape_inference.infer_shapes_path(output_path, output_path)
 
+    # Compare output with torch model and ONNX model
+    try:
+        torch_out = model(img).numpy()
+        session = onnxruntime.InferenceSession(output_path, providers=['CPUExecutionProvider'])
+        onnx_out = session.run(None, {input_names[0]: img.numpy()})[0]
+        np.testing.assert_allclose(torch_out, onnx_out, rtol=1e-3, atol=1e-5)
+    except AssertionError as e:
+        print(e)
+        stdin = input('Do you want to ignore the error and proceed with the export ([y]/n)? ')
+        if stdin == 'n':
+            os.remove(output_path)
+            exit(1)
 
-    # onnx infer
-    if opt.onnx_infer:
-        import onnxruntime
-        import numpy as np
-        providers =  ['CPUExecutionProvider']
-        session = onnxruntime.InferenceSession(f, providers=providers)
-        im = img.cpu().numpy().astype(np.float32) # torch to numpy
-        y_onnx = session.run([session.get_outputs()[0].name], {session.get_inputs()[0].name: im})[0]
-        print("pred's shape is ",y_onnx.shape)
-        print("max(|torch_pred - onnx_pred|） =",abs(y.cpu().numpy()-y_onnx).max())
-
+    # Simplify ONNX model
+    if not opt.skip_simplify:
+        model = onnx.load(output_path)
+        input_shapes = {model.graph.input[0].name: img.shape}
+        model, check = onnxsim.simplify(model, test_input_shapes=input_shapes)
+        assert check, 'Simplified ONNX model could not be validated'
+        onnx.save(model, output_path)
+    print(f'Successfully export ONNX model: {output_path}')
 
     # TensorRT export
     if opt.onnx2trt:
         from torch2trt.trt_model import ONNX_to_TRT
-        print('\nStarting TensorRT...')
-        ONNX_to_TRT(onnx_model_path=f,trt_engine_path=f.replace('.onnx', '.trt'),fp16_mode=opt.fp16_trt)
 
-    # PB export
-    if opt.onnx2pb:
-        print('download the newest onnx_tf by https://github.com/onnx/onnx-tensorflow/tree/master/onnx_tf')
-        from onnx_tf.backend import prepare
-        import tensorflow as tf
-
-        outpb = f.replace('.onnx', '.pb')  # filename
-        # strict=True maybe leads to KeyError: 'pyfunc_0', check: https://github.com/onnx/onnx-tensorflow/issues/167
-        tf_rep = prepare(onnx_model, strict=False)  # prepare tf representation
-        tf_rep.export_graph(outpb)  # export the model
-
-        out_onnx = tf_rep.run(img) # onnx output
-
-        # check pb
-        with tf.Graph().as_default():
-            graph_def = tf.GraphDef()
-            with open(outpb, "rb") as f:
-                graph_def.ParseFromString(f.read())
-                tf.import_graph_def(graph_def, name="")
-            with tf.Session() as sess:
-                init = tf.global_variables_initializer()
-                input_x = sess.graph.get_tensor_by_name(input_names[0]+':0')  # input
-                outputs = []
-                for i in output_names:
-                    outputs.append(sess.graph.get_tensor_by_name(i+':0'))
-                out_pb = sess.run(outputs, feed_dict={input_x: img})
-
-        print(f'out_pytorch {y}')
-        print(f'out_onnx {out_onnx}')
-        print(f'out_pb {out_pb}')
+        print('\nStarting TensorRT export...')
+        ONNX_to_TRT(output_path, output_path.replace('.onnx', '.trt'), fp16_mode=opt.fp16_trt)
